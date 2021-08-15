5522b6cefe2eb46603f723a67bd55e30da3b47f4
Indiarto Aji Begawan <indizi130899@gmail.com>
2021-08-15 20:25:17 +0700

    edit _header2.php

--- SourceCode2/application/views/halamanpelanggan/Deera.php
+++ SourceCode2/application/views/halamanpelanggan/Deera.php
@@ -2,174 +2,188 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Deera  |  IDNFT</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+  <title>Deera | IDNFT</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    <!-- Favicons -->
-    <link href="<?= base_url()?>assets/img/LogoIDNFT.png" rel="icon">
-    <link href="<?= base_url()?>assets/img/LogoIDNFT.png" rel="apple-touch-icon">
+  <!-- Favicons -->
+  <link href="<?= base_url() ?>assets/img/LogoIDNFT.png" rel="icon">
+  <link href="<?= base_url() ?>assets/img/LogoIDNFT.png" rel="apple-touch-icon">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="<?= base_url()?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= base_url()?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-    <link href="<?= base_url()?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="<?= base_url()?>assets/vendor/venobox/venobox.css" rel="stylesheet">
-    <link href="<?= base_url()?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?= base_url() ?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
-    <link href="<?= base_url()?>assets/css/style.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
 
-    <link rel="icon" type="image/png" href="https://www.weebly.com/uploads/reseller/assets/438585504-favicon.ico">
+  <link rel="icon" type="image/png" href="https://www.weebly.com/uploads/reseller/assets/438585504-favicon.ico">
 
 </head>
 
 <body>
-    <!-- Header -->
-	 <?php require_once('application\views\template\_header.php'); ?>
-  	<!-- End Header -->
+  <!-- Header -->
+  <?php require_once('application\views\template\_header.php'); ?>
+  <!-- End Header -->
 
-	  <main id="main">
+  <main id="main">
 
     <!-- ======= Breadcrumbs ======= -->
     <section class="breadcrumbs">
-    <div class="container">
+      <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-        <h2>Deera Page</h2>
+          <h2>Deera Page</h2>
         </div>
 
-    </div>
+      </div>
     </section><!-- End Breadcrumbs -->
 
     <section class="inner-page">
-    <div class="container" >
+      <div class="container">
 
-        <h2 style="text-align:center;"><font size="20">IDNFT DEERA PROJECT&nbsp;</font></h2>
-        <h2 style="text-align:center;"><font color="black" size="5">PENDAFTARAN KOLABORASI IDNFT X 76 INDONESIAN NFT ARTIST</font></h2>
+        <h2 style="text-align:center;">
+          <font size="20">IDNFT DEERA PROJECT&nbsp;</font>
+        </h2>
+        <h2 style="text-align:center;">
+          <font color="black" size="5">PENDAFTARAN KOLABORASI IDNFT X 76 INDONESIAN NFT ARTIST</font>
+        </h2>
 
-            <div class="paragraph" style="text-align:justify"><font size="3">Hi Fam, selamat datang di ​seleksi kolaborasi IDNFT x NFT Artist Indonesia dalam rangka memperingati HUT RI 76. Kolaborasi ini berupa pembuatan NFT Collectible Items dengan menggunakan karakter "Rusa Bawen" yang bernama DEERA. Setiap artist terpilih akan mendapatkan satu karakter dengan mock-up dari karya masing-masing artist. Untuk artist 2D proses mock-up akan dibantu oleh tim IDNFT. Untuk Artist 3D akan diberikan file dan guidline untuk bisa diolah sendiri.&nbsp;<br>&nbsp;</div>
-            <div class="paragraph" style="text-align:left"><strong>Ketentuan Fee Penjualan:</strong>
-                    <ol>
-                        <li>Karya akan mint di akun resmi IDNFT di ETH Platforms (Opensea/lainnya)</li>
-                        <li>Wallet akan di-split antara IDNFT dan artist. Jika terjadi penjualan, otomatis artist akan mendapatkan share 60% di&nbsp;akun walletnya, begitu juga dengan penjualan di secondary market.</li>
-                        <li>Peserta tidak dikenakan biaya apapun</li>
-                        <li>Detail pembagian fee project sebagai berikut:</li>
-                    </ol>
-                </font>
-            </div>
+        <div class="paragraph" style="text-align:justify">
+          <font size="3">Hi Fam, selamat datang di ​seleksi kolaborasi IDNFT x NFT Artist Indonesia dalam rangka memperingati HUT RI 76. Kolaborasi ini berupa pembuatan NFT Collectible Items dengan menggunakan karakter "Rusa Bawen" yang bernama DEERA. Setiap artist terpilih akan mendapatkan satu karakter dengan mock-up dari karya masing-masing artist. Untuk artist 2D proses mock-up akan dibantu oleh tim IDNFT. Untuk Artist 3D akan diberikan file dan guidline untuk bisa diolah sendiri.&nbsp;<br>&nbsp;
+        </div>
+        <div class="paragraph" style="text-align:left"><strong>Ketentuan Fee Penjualan:</strong>
+          <ol>
+            <li>Karya akan mint di akun resmi IDNFT di ETH Platforms (Opensea/lainnya)</li>
+            <li>Wallet akan di-split antara IDNFT dan artist. Jika terjadi penjualan, otomatis artist akan mendapatkan share 60% di&nbsp;akun walletnya, begitu juga dengan penjualan di secondary market.</li>
+            <li>Peserta tidak dikenakan biaya apapun</li>
+            <li>Detail pembagian fee project sebagai berikut:</li>
+          </ol>
+          </font>
+        </div>
 
-            <div>
-                <div class="wsite-image wsite-image-border-medium " style="padding-top:5px;padding-bottom:10px;margin-left:0px;margin-right:10px;text-align:center">
-                    <a>
-                        <img src="<?= base_url() ?>assets\img\Deera01.jpg" alt="Picture" style="width:auto;max-width:60%;">
-                    </a>
+        <div>
+          <div class="wsite-image wsite-image-border-medium " style="padding-top:5px;padding-bottom:10px;margin-left:0px;margin-right:10px;text-align:center">
+            <a>
+              <img src="<?= base_url() ?>assets\img\Deera01.jpg" alt="Picture" style="width:auto;max-width:60%;">
+            </a>
             <div style="display:block;font-size:90%"></div>
-                </div>
-            </div>
+          </div>
+        </div>
 
-            <div class="paragraph" style="text-align:left"><font size="3"><strong>Ketentuan Karya</strong>
-                    <ol style="color:rgb(42, 42, 42)">
-                        <li>Karya harus original (IDNFT tidak bertanggungjawab terhadap karya yang mengandung unsur plagiat atau hal lain yang melanggar hukum)</li>
-                        <li>Artist diperbolehkan menggunakan karya lama/baru</li>
-                        <li>Karya yang dikirim belum di mint di blockchain market manapun</li>
-                        <li>2D Artist mengirimkan file dalam bentuk JPEG/PNG dengan resolusi minimal 3000x3750p (4:5 portrait).&nbsp;</li>
-                        <li>Hasil mock-up sepenuhnya menjadi kewenangan dari tim IDNFT</li>
-                        <li>3D Artist mengikuti ketentuan yang ada di bawah</li>
-                        <li>Format nama karakter dari masing-masing artist “DEERA+Nomor Unik”</li>
-                        <li>Proses seleksi artist sepenuhnya menjadi kewenangan dari pihak&nbsp;IDNFT&nbsp;</li>
-                        <li>Pendaftaran + submit karya maksimal dikirimkan pada tanggal 17&nbsp;Agustus 2021</li>
-                        <li>Pengumuman seleksi akan di udate setelahnya</li>
-                    </ol>  
-                </font>
-            </div>
+        <div class="paragraph" style="text-align:left">
+          <font size="3"><strong>Ketentuan Karya</strong>
+            <ol style="color:rgb(42, 42, 42)">
+              <li>Karya harus original (IDNFT tidak bertanggungjawab terhadap karya yang mengandung unsur plagiat atau hal lain yang melanggar hukum)</li>
+              <li>Artist diperbolehkan menggunakan karya lama/baru</li>
+              <li>Karya yang dikirim belum di mint di blockchain market manapun</li>
+              <li>2D Artist mengirimkan file dalam bentuk JPEG/PNG dengan resolusi minimal 3000x3750p (4:5 portrait).&nbsp;</li>
+              <li>Hasil mock-up sepenuhnya menjadi kewenangan dari tim IDNFT</li>
+              <li>3D Artist mengikuti ketentuan yang ada di bawah</li>
+              <li>Format nama karakter dari masing-masing artist “DEERA+Nomor Unik”</li>
+              <li>Proses seleksi artist sepenuhnya menjadi kewenangan dari pihak&nbsp;IDNFT&nbsp;</li>
+              <li>Pendaftaran + submit karya maksimal dikirimkan pada tanggal 17&nbsp;Agustus 2021</li>
+              <li>Pengumuman seleksi akan di udate setelahnya</li>
+            </ol>
+          </font>
+        </div>
 
-            <div class="paragraph" style="text-align:left color:black"><font size="3"><strong>​Ketentuan 3D Artist</strong>
-                    <ol style="color:rgb(42, 42, 42)">
-                        <li>3D Artist dapat mengunduh file di link:&nbsp;<a href="https://drive.google.com/drive/folders/1VtppkIUEmCQhKWuMHBi6qJQqTdvvarSX?usp=sharing" target="_blank">Download</a></li>
-                        <li>3D Artist boleh menambahkan object, pernak-pernik disekitar model, dan bebas menyesuaikan background sesuai karakter masing-masing artist.</li>
-                        <li>3D Artist&nbsp;&nbsp;tidak diperbolehkan merubah angle camera, merubah/mengurangi basic model dari karakter, merubah posisi, skala dan rotasi dari karakter DEERA.</li>
-                        <li>3D Artist mengikuti view kamera pada file dari IDNFT dengan output image 3000x3750p (portrait) dengan format Image .PNG</li>
-                    </ol>
-                </font>
-                <span style="color:black"><font size="4"><strong>​Note: Bagi peserta yang tidak lolos DILARANG menggunakan karakter DEERA untuk di posting atau untuk keperluan apapun.</strong></span>
-            </div>
+        <div class="paragraph" style="text-align:left color:black">
+          <font size="3"><strong>​Ketentuan 3D Artist</strong>
+            <ol style="color:rgb(42, 42, 42)">
+              <li>3D Artist dapat mengunduh file di link:&nbsp;<a href="https://drive.google.com/drive/folders/1VtppkIUEmCQhKWuMHBi6qJQqTdvvarSX?usp=sharing" target="_blank">Download</a></li>
+              <li>3D Artist boleh menambahkan object, pernak-pernik disekitar model, dan bebas menyesuaikan background sesuai karakter masing-masing artist.</li>
+              <li>3D Artist&nbsp;&nbsp;tidak diperbolehkan merubah angle camera, merubah/mengurangi basic model dari karakter, merubah posisi, skala dan rotasi dari karakter DEERA.</li>
+              <li>3D Artist mengikuti view kamera pada file dari IDNFT dengan output image 3000x3750p (portrait) dengan format Image .PNG</li>
+            </ol>
+          </font>
+          <span style="color:black">
+            <font size="4"><strong>​Note: Bagi peserta yang tidak lolos DILARANG menggunakan karakter DEERA untuk di posting atau untuk keperluan apapun.</strong>
+          </span>
+        </div>
 
-                <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0;margin-right:0;text-align:center">
-                    <a>
-                        <img src="<?= base_url() ?>assets\img\Deera01.jpg" alt="Picture" style="width:auto;max-width:60%;">
-                    </a>
-                <div style="display:block;font-size:90%"></div>
-                </div>
-            </div>
+        <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0;margin-right:0;text-align:center">
+          <a>
+            <img src="<?= base_url() ?>assets\img\Deera01.jpg" alt="Picture" style="width:auto;max-width:60%;">
+          </a>
+          <div style="display:block;font-size:90%"></div>
+        </div>
+      </div>
 
-        <h2 class="wsite-content-title" style="text-align:center;"><a href="https://forms.gle/JCfBG2GF5C5rokbz8" target="_blank">DAFTAR SEKARANG</a></h2>
-        <h2 class="wsite-content-title" style="text-align:center;"><font size="4">Let's to be part of our movement</a></h2>
-    </script>
-</div>
-</section>
+      <h2 class="wsite-content-title" style="text-align:center;"><a href="https://forms.gle/JCfBG2GF5C5rokbz8" target="_blank">DAFTAR SEKARANG</a></h2>
+      <h2 class="wsite-content-title" style="text-align:center;">
+        <font size="4">Let's to be part of our movement</a>
+      </h2>
+      </script>
+      </div>
+    </section>
 
-</main><!-- End #main -->
+  </main><!-- End #main -->
 
-<!-- ======= Footer ======= -->
-<footer id="footer">
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
 
-<div class="footer-top">
+    <div class="footer-top">
 
-  <div class="container">
+      <div class="container">
 
-	<div class="row  justify-content-center">
-	<div class="col-lg-6">
-		<h3>IDNFT</h3>
-		<p>Let’s Make Art Alive !
-			Have questions?</p>
-	  </div>
-	</div>
+        <div class="row  justify-content-center">
+          <div class="col-lg-6">
+            <h3>IDNFT</h3>
+            <p>Let’s Make Art Alive !
+              Have questions?</p>
+          </div>
+        </div>
 
-	<div class="row footer-newsletter justify-content-center">
-	  <div class="col-lg-6">
-		<form action="" method="post"> 
-		  <input type="email" name="email" placeholder="Enter your Email"><input type="submit" value="Subscribe">
-		</form>
-	  </div>
-	</div>
+        <div class="row footer-newsletter justify-content-center">
+          <div class="col-lg-6">
+            <form action="" method="post">
+              <input type="email" name="email" placeholder="Enter your Email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
 
-	<div class="social-links">
-	  <a href="https://twitter.com/idnft_" class="twitter"><i class="bx bxl-twitter"></i></a>
-	  <a href="https://www.instagram.com/id.nft/" class="instagram"><i class="bx bxl-instagram"></i></a>
-	  <a href="https://discord.gg/aDtCQEE7" class="google-plus"><i class="bx bxl-discord"></i></a>
-	</div>
-  </div>
-</div>
+        <div class="social-links">
+          <a href="https://twitter.com/idnft_" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="https://www.instagram.com/id.nft/" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="https://discord.gg/aDtCQEE7" class="google-plus"><i class="bx bxl-discord"></i></a>
+        </div>
+      </div>
+    </div>
 
-<div class="container footer-bottom clearfix">
-<div class="copyright">
-	&copy;<script>document.write(new Date().getFullYear());</script> Copyright <strong><span>IDNFT</span></strong>. All Rights Reserved
-  </div>
-</div>
-</footer><!-- End Footer -->
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy;<script>
+          document.write(new Date().getFullYear());
+        </script> Copyright <strong><span>IDNFT</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
 
-<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="<?= base_url()?>assets/vendor/jquery/jquery.min.js"></script>
-  <script src="<?= base_url()?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?= base_url()?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="<?= base_url()?>assets/vendor/php-email-form/validate.js"></script>
-  <script src="<?= base_url()?>assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="<?= base_url()?>assets/vendor/counterup/counterup.min.js"></script>
-  <script src="<?= base_url()?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="<?= base_url()?>assets/vendor/venobox/venobox.min.js"></script>
-  <script src="<?= base_url()?>assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/counterup/counterup.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/venobox/venobox.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/owl.carousel/owl.carousel.min.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="<?= base_url()?>assets/js/main.js"></script>
+  <script src="<?= base_url() ?>assets/js/main.js"></script>
 
 </body>
 
