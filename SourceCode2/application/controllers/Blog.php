04080c981dbaed94183fcbfafd4cc6dd1eee652b
DevitaDwitama136 <devitadwitama18@if.unjani.ac.id>
2021-08-16 19:19:00 +0700

    UPDATE CONTROLLER BLOG DAN HOME
    Soalnya ada tambahan artikel jadi controllernya ubah, sama home

--- SourceCode2/application/controllers/Blog.php
+++ SourceCode2/application/controllers/Blog.php
@@ -13,5 +13,8 @@
         public function Artikel2(){
             $this->load->view("halamanpelanggan/Artikel2");
         }
+        public function Artikel3(){
+            $this->load->view("halamanpelanggan/Artikel3");
+        }
 
     }
