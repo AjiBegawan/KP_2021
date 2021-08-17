8a50fa2e2c7b035ccfe4c160cda55fa7d82c4fca
Indiarto Aji Begawan <indizi130899@gmail.com>
2021-08-17 11:27:36 +0700

    Add Artist of The Month

--- SourceCode2/application/views/halamanpelanggan/home.php
+++ SourceCode2/application/views/halamanpelanggan/home.php
@@ -14,10 +14,9 @@
   <link href="<?= base_url() ?>assets/img/LogoIDNFT.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
   <!-- Vendor CSS Files -->
   <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?= base_url() ?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
@@ -54,16 +53,16 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="<?= base_url() ?>assets\img\Header04.jpg" alt="carousel1" style="width: 100%;min-width: 100%;">
+        <img src="<?= base_url() ?>assets\img\Home\Header.jpg" alt="carousel1" style="width: 100%;min-width: 100%;">
 
       </div>
 
       <div class="item">
-        <img src="<?= base_url() ?>assets\img\Header04.jpg" alt="carousel2" style="width: 100%;">
+        <img src="<?= base_url() ?>assets\img\Home\Header.jpg" alt="carousel2" style="width: 100%;">
       </div>
 
       <div class="item">
-        <img src="<?= base_url() ?>assets\img\Header04.jpg" alt="carousel3" style="width: 100%;">
+        <img src="<?= base_url() ?>assets\img\Home\Header.jpg" alt="carousel3" style="width: 100%;">
       </div>
     </div>
 
@@ -95,49 +94,38 @@
         <div class="row">
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
             <div class="member">
-              <img src="<?= base_url() ?>assets/img/team/Person2.jpg" alt="">
-              <h4>Diandra Syarrofa</h4>
-              <span>Mural Art Designer</span>
-              <span>Yogjakarta, DIY</span>
+              <img src="<?= base_url("assets\img\AOTM\RifqiRubahHitam.png") ?>" alt="">
+              <h3>Rifqi Rubah Hitam</h3>
+              <span><h5>Motion Designer | 2D Animator | 3D Artist</h5> </span>
+              <span><i class="icofont-location-pin"></i>Yogjakarta, Indonesia</span>
               <p>
 
               </p>
               <div class="social">
-                <a href=""><i class="icofont-twitter"></i></a>
-                <a href=""><i class="icofont-facebook"></i></a>
-                <a href="https://instagram.com/dvtadwitama"><i class="icofont-instagram"></i></a>
-                <a href=""><i class="icofont-linkedin"></i></a>
+                <a href="https://twitter.com/rubahitam"><i class="icofont-twitter"></i></a>
+                <a href="https://www.instagram.com/rubahitam_/"><i class="icofont-instagram"></i></a>
+                <a href="https://lynkfire.com/rubahitam"><i class="icofont-web"></i></a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
             <div class="member">
-              <img src="<?= base_url() ?>assets/img/team/Person1.jpg" alt="">
-              <h4>Devita Dwitama Putri Baron</h4>
-              <span>Digital Illuslator</span>
-              <span>Surabaya, Jawa Tengah</span>
-              <p>
-
-              </p>
+              <img src="<?= base_url("assets\img\AOTM\Suryanto.png") ?>" alt="">
+              <h4>Suryanto Sur</h4>
+              <span><i class="icofont-location-pin"></i>Indonesia</span>
               <div class="social">
-                <a href=""><i class="icofont-twitter"></i></a>
-                <a href=""><i class="icofont-facebook"></i></a>
-                <a href="https://instagram.com/dvtadwitama"><i class="icofont-instagram"></i></a>
-                <a href=""><i class="icofont-linkedin"></i></a>
+                <a href="https://twitter.com/Suryanto_sur234"><i class="icofont-twitter"></i></a>
+                <a href="https://www.instagram.com/suryanto123/"><i class="icofont-instagram"></i></a>
               </div>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
             <div class="member">
-              <img src="<?= base_url() ?>assets/img/team/Person2.jpg" alt="">
-              <h4>Indiarto Aji Begawan</h4>
-              <span>Artist</span>
-              <span>Bandung, Jawa Barat</span>
-              <p>
-
-              </p>
+              <img src="<?= base_url("assets\img\AOTM\Tagpaw.png") ?>" alt="">
+              <h4>Tagpaw</h4>
+              <span><i class="icofont-location-pin"></i>Jakarta, Indonesia</span>
               <div class="social">
                 <a href=""><i class="icofont-twitter"></i></a>
                 <a href=""><i class="icofont-facebook"></i></a>
@@ -179,33 +167,23 @@
         </div>
       </div>
     </section><!-- Benefit Section -->
+
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-4" style="width: 18rem;">
+        <a href="<?php echo site_url('Home/BlogView') ?>" class=" ">
+          <img class="card-img-top" src="<?= base_url("assets\img\Home\Web01.jpg") ?>" alt="Card image cap">
+        </a>
+      </div>
+      <div class="col-sm-2"></div>
+      <div class="col-sm-4" style="width: 18rem;">
+        <a href="<?php echo site_url('Home/BlogView') ?>" class=" ">
+          <img class="card-img-top" src="<?= base_url("assets\img\Home\Web02.jpg") ?>" alt="Card image cap">
+        </a>
+      </div>
+      <div class="col-sm-1"></div>
+    </div>
   </main><!-- End #main -->
-
-  <div class="row">
-    <div class="col-sm-1"></div>
-    <div class="card col-sm-4" style="width: 18rem;">
-      <img class="card-img-top" src="<?= base_url("assets\image\bg_blog.jpg") ?>" alt="Card image cap">
-      <div class="card-body  text-center">
-        <h5 class="card-title">New in NFT ?</h5>
-        <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-        <a href="<?php echo site_url('Home/BlogView') ?>" class="btn btn-primary ">Click here !</a>
-      </div>
-    </div>
-    <div class="col-sm-2"></div>
-    <div class="card col-sm-4" style="width: 18rem;">
-      <img class="card-img-top" src="<?= base_url("assets\image\bg_blog.jpg") ?>" alt="Card image cap">
-      <div class="card-body  text-center">
-        <h5 class="card-title">Want to join live class NFT ?</h5>
-        <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-        <a href="<?= site_url("/SignUp") ?>" class="btn btn-primary ">Join Us</a>
-      </div>
-    </div>
-    <div class="col-sm-1"></div>
-  </div>
-
-
-
-
 
   <!-- ======= Footer ======= -->
   <?php require_once('application\views\template\_footer.php'); ?>
