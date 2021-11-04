9b63a236b148df30a81c3004891b0ba70cefa895
Indiarto Aji Begawan <indizi130899@gmail.com>
2021-11-04 07:49:37 +0700

    Update database.php

--- SourceCode2Up/application/config/database.php
+++ SourceCode2Up/application/config/database.php
@@ -94,3 +94,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'sql108.epizy.com',
+// 	'username' => 'epiz_30260693',
+// 	'password' => 'HHIudrFWpY6',
+// 	'database' => 'epiz_30260693_idnft',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
