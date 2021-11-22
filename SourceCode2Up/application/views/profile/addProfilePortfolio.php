613df05ac2a83e0e8019237b4c16cf1c1c216cd8
Indiarto Aji Begawan <indizi130899@gmail.com>
2021-11-22 18:02:57 +0700

    upload karya ajibegawan dan indiartoajibegawan

--- SourceCode2Up/application/views/profile/addProfilePortfolio.php
+++ SourceCode2Up/application/views/profile/addProfilePortfolio.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Edit User | IDNFT </title>
+    <title>Tambah Karya | IDNFT </title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
